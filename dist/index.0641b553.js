16962435ec9f5c6a9e7e9f880becd483f1dcf3c6
KonyEnBikini <nicolas.borlet7@gmail.com>
2022-09-27 14:44:00 +0200

    Start js list category

--- dist/index.0641b553.js
+++ dist/index.0641b553.js
@@ -535,20 +535,45 @@ function hmrAcceptRun(bundle, id) {
 require("dotenv").config({
     path: "../.env"
 });
-fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=d52f854a2c7af4dfc18f4b5045adb81e").then((res)=>res.json()).then((data)=>{
-    const genre = data.genres.map((g)=>g.name);
-    const listGenre = genre.map((element)=>{
-        return element;
+const CategoriesListing = ()=>{
+    fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=d52f854a2c7af4dfc18f4b5045adb81e").then((res)=>res.json()).then((data)=>{
+        const genre = data.genres.map((g)=>g.name);
+        const listGenre = genre.map((element)=>{
+            return element;
+        });
+        listGenre.forEach((element)=>{
+            //   <div class="container">
+            //   <div class="container-info">
+            //     <h2>Test</h2>
+            //     <p class="film-date">Test</p>
+            //     <p class="film-realisateur">Test</p>
+            //     <p class="film-acteurs">Test</p>
+            //   </div>
+            //   <div class="container-img">
+            //     <img src="https://www.referenseo.com/wp-content/uploads/2019/03/image-attractive.jpg" alt="maxi éléphant"/>
+            //   </div>
+            // </div>
+            console.log(element);
+            // ✅ Create element
+            const containerDOM = document.createElement("div");
+            containerDOM.setAttribute("class", "container");
+            const containerInfoDOM = document.createElement("div");
+            containerInfoDOM.setAttribute("class", "container");
+            // ✅ Add text content to element
+            // div.textContent = JSON.stringify(listGenre);
+            // ✅ Or set the innerHTML of the element
+            // ✅ (Optionally) Set Attributes on Element
+            // div.setAttribute('title', 'my-title');
+            // ✅ (Optionally) Set styles on Element
+            // el.style.backgroundColor = 'salmon';
+            // el.style.color = 'white';
+            // ✅ add element to DOM
+            const container = document.querySelector(".block-container");
+            container.appendChild(containerDOM);
+        });
     });
-    const div = document.createElement("div");
-    array.forEach((element)=>{
-        div;
-    });
-    Genre.forEach((element)=>{
-        console.log(element);
-        document.getElementById("demo").innerHTML = element;
-    });
-});
+};
+CategoriesListing();
 
 },{"dotenv":"lErsX"}],"lErsX":[function(require,module,exports) {
 var process = require("process");
--- src/app.js
+++ src/app.js
@@ -1,13 +1,53 @@
 require("dotenv").config({ path: "../.env"});
 
-fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=" + process.env.APP_CLE_API)
+const CategoriesListing = () => {
+    fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=" + process.env.APP_CLE_API)
     .then(res => res.json())
     .then ( data => {
         const genre=('res', data.genres.map(g => g.name))
         const listGenre=genre.map(element => {
             return element
         })
-        const div = document.createElement("div");
-        listGenre.forEach(element => { 
+        listGenre.forEach(element => {
+
+    //   <div class="container">
+    //   <div class="container-info">
+    //     <h2>Test</h2>
+    //     <p class="film-date">Test</p>
+    //     <p class="film-realisateur">Test</p>
+    //     <p class="film-acteurs">Test</p>
+    //   </div>
+    //   <div class="container-img">
+    //     <img src="https://www.referenseo.com/wp-content/uploads/2019/03/image-attractive.jpg" alt="maxi éléphant"/>
+    //   </div>
+    // </div>
+            console.log(element)
+
+            // ✅ Create element
+            const containerDOM = document.createElement('div');
+            containerDOM.setAttribute('class', "container");
+            
+            const containerInfoDOM = document.createElement('div');
+            containerInfoDOM.setAttribute('class', "container");
+
+
+            // ✅ Add text content to element
+            // div.textContent = JSON.stringify(listGenre);
+
+            // ✅ Or set the innerHTML of the element
+
+            // ✅ (Optionally) Set Attributes on Element
+            // div.setAttribute('title', 'my-title');
+
+            // ✅ (Optionally) Set styles on Element
+            // el.style.backgroundColor = 'salmon';
+            // el.style.color = 'white';
+
+            // ✅ add element to DOM
+            const container = document.querySelector('.block-container');
+            container.appendChild(containerDOM);
         });
     })
+}
+
+CategoriesListing();
