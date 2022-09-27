550049f3b40031c19b6a461758b097f067befe1b
KonyEnBikini <nicolas.borlet7@gmail.com>
2022-09-27 15:21:09 +0200

    Fix app.js

--- src/app.js
+++ src/app.js
@@ -1,54 +1,15 @@
-require("dotenv").config({ path: "../.env"});
+require('dotenv').config({path:".env"})
 
-const CategoriesListing = () => {
-    fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=" + process.env.APP_CLE_API)
-    .then(res => res.json())
-    .then ( data => {
-        const genre=('res', data.genres.map(g => g.name))
-        const listGenre=genre.map(element => {
-            return element
-        })
-        listGenre.forEach(element => {
-
-    //   <div class="container">
-    //   <div class="container-info">
-    //     <h2>Test</h2>
-    //     <p class="film-date">Test</p>
-    //     <p class="film-realisateur">Test</p>
-    //     <p class="film-acteurs">Test</p>
-    //   </div>
-    //   <div class="container-img">
-    //     <img src="https://www.referenseo.com/wp-content/uploads/2019/03/image-attractive.jpg" alt="maxi éléphant"/>
-    //   </div>
-    // </div>
-            console.log(element)
-
-            // ✅ Create element
-            const containerDOM = document.createElement('div');
-            containerDOM.setAttribute('class', "container");
-            
-            const containerInfoDOM = document.createElement('div');
-            containerInfoDOM.setAttribute('class', "container-info");
-
-            
-            // ✅ Add text content to element
-            // div.textContent = JSON.stringify(listGenre);
-
-            // ✅ Or set the innerHTML of the element
-            containerDOM.innerHTML = element;
-
-            // ✅ (Optionally) Set Attributes on Element
-            // div.setAttribute('title', 'my-title');
-
-            // ✅ (Optionally) Set styles on Element
-            // el.style.backgroundColor = 'salmon';
-            // el.style.color = 'white';
-
-            // ✅ add element to DOM
-            const container = document.querySelector('.block-container');
-            container.appendChild(containerDOM);
-        });
-    })
+const myFetch = (path) => {
+    return new Promise( (resolve, reject) => {
+        fetch("https://api.themoviedb.org/3" + path + "?api_key=" + process.env.APP_CLE_API + "&query=Jack+Reacher")
+            .then(res => res.json())
+            .then(data => resolve(data))
+            .catch(e => reject(e))
+    }) 
 }
 
-CategoriesListing();
+myFetch("/search/movie")
+    .then(data=> {
+        console.log('res',data)
+    })
