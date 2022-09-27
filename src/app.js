71b37de584f1b961c7acdb7d6dd58d06c17bd144
KonyEnBikini <nicolas.borlet7@gmail.com>
2022-09-27 15:33:49 +0200

    MAJ

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 require('dotenv').config({path:".env"})
 
-const myFetch = (path) => {
+const MovieListing = (path) => {
     return new Promise( (resolve, reject) => {
         fetch("https://api.themoviedb.org/3" + path + "?api_key=" + process.env.APP_CLE_API + "&query=Jack+Reacher")
             .then(res => res.json())
@@ -9,7 +9,22 @@ const myFetch = (path) => {
     }) 
 }
 
-myFetch("/search/movie")
+MovieListing("/search/movie")
     .then(data=> {
-        console.log('res',data)
+        const genre=('res', data.results.map(g => g.name))
+        const listGenre=genre.map(element => {
+            return element
+        })
+        
+        listGenre.forEach(element => {
+        
+        const containerDOM = document.createElement('div');
+        containerDOM.setAttribute('class', "container");
+
+        const containerInfoDOM = document.createElement('div');
+        containerInfoDOM.setAttribute('class', "container-info");
+
+        const container = document.querySelector('.block-container');
+        container.appendChild(containerDOM);
+        })
     })
