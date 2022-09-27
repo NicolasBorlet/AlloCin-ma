e6a39c4f4cf0754a4cd48a5f1acda96cf0622f46
KonyEnBikini <nicolas.borlet7@gmail.com>
2022-09-27 10:11:02 +0200

    .

--- src/app.js
+++ src/app.js
@@ -3,11 +3,12 @@ require("dotenv").config({ path: "../.env"});
 fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=" + process.env.APP_CLE_API)
     .then(res => res.json())
     .then ( data => {
-        Genre=('res', data.genres.map(g => g.name))
+        const genre=('res', data.genres.map(g => g.name))
+        const listGenre=genre.map(element => {
+            return element
+        })
+        const div = document.createElement("div");
 
-
-        Genre.forEach(element => {
-            console.log(element)
-            document.getElementById("demo").innerHTML = element;
-        });
+        document.getElementById("demo").innerHTML = JSON.stringify(listGenre);
+        //document.getElementById("main").appendChild(div).innerHTML = JSON.stringify(listGenre);
     })
